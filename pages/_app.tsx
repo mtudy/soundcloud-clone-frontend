2f8677690d0c42625e6c8e84f5bfd429c07e8ef2
jeongmyeong <kimbym23@gmail.com>
2020-12-10 23:09:31 +0900

    install eslint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 // import App from "next/app";
-import type { AppProps /*, AppContext */ } from 'next/app'
+import type { AppProps /* , AppContext */ } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
