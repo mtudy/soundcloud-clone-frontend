3dea71081cc1bdf5d0c3b1d648496bd3a436639b
Surtr <cherryred5959@gmail.com>
2020-12-12 04:07:41 +0900

    connect graphql

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,19 @@ import '../styles/globals.css';
 // import App from "next/app";
 import type { AppProps /* , AppContext */ } from 'next/app';
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'localhost:8080',
+  cache: new InMemoryCache(),
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
